e4463dbfbc1786e2e77f84ed83d7c674f36cd26e
Leo <barberlt1@appstate.edu>
2024-05-30 11:19:47 -0400

    Added more text

--- Hello.java
+++ Hello.java
@@ -7,4 +7,7 @@ public class Hello
     // Green means it is a new file in the git depository.
     // When doing Git add, it will switch to A meaning it has been added.
     // When doing commit, it will go away, meaning it is up to date with the last commit.
+    // After editing since last commit, it will switch to M (modified).
+
+    // Powershell is being used to connect to Git. You don't have to use Git bash.
 }
