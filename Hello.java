650ea141c3d14edbfc7a97ce145334cf03e7e9ac
Leo <barberlt1@appstate.edu>
2024-05-30 11:19:26 -0400

    Created Hellow World Program in class

---
+++ Hello.java
@@ -0,0 +1,10 @@
+public class Hello
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+    }
+    // Green means it is a new file in the git depository.
+    // When doing Git add, it will switch to A meaning it has been added.
+    // When doing commit, it will go away, meaning it is up to date with the last commit.
+}
